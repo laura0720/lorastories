d8f9b110457d029826ecaa8677b53507143992f0
Laura <lauzhenisova@gmail.com>
2017-03-12 23:18:31 +0600

    first blood

---
+++ application/views/pages/index.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+	$("#accordion").accordion({
+		collapsible: true
+	});
+	$( "#tabs" ).tabs({
+      	event: "mouseover",
+      	heightStyle: "content"
+    });
+        $( document ).tooltip();
+  } );
+
+
+
+
+
